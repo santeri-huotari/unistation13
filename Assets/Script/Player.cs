2cb12bfa90daecda2206b99c68d56fe7e1aebc1a
lanu <lassi.laiho0@gmail.com>
2016-11-22 17:55:18 +0200

    Added basic functionality

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed = 300;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Movement
+		float xdir = Input.GetAxisRaw("Horizontal");
+		float ydir = Input.GetAxisRaw("Vertical");
+		rb.velocity = new Vector2(speed * xdir * Time.deltaTime, speed *  ydir * Time.deltaTime);
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # unistation13
 Space Station 13 remake made in Unity.
+
+Graphics and sounds are from tgstation (https://github.com/tgstation/tgstation).
