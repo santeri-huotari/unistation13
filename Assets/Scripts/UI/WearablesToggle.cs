a864a60008fdbebbc0d95fde4072001247327e4f
Lassi Laiho <lassi.laiho0@gmail.com>
2017-03-08 17:37:14 +0200

    Restructured folder layout

---
+++ Assets/Scripts/UI/WearablesToggle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WearablesToggle : MonoBehaviour {
+	public GameObject wearableSlots;
+
+	public void toggleEquipmentSlots() {
+		wearableSlots.SetActive(!wearableSlots.activeSelf);
+	}
+}
